c117fc01dcac1d5b68978b4cf31506744ce72ab2
Matteo-Nesti <matteo.nesti@hotmail.com>
2023-07-05 17:15:39 +0200

    add series

--- src/App.vue
+++ src/App.vue
@@ -2,6 +2,7 @@
   <div>
     <input type="text" v-model="searchTerm">
     <button @click="onSearchTerm(searchTerm)">Cerca</button>
+    <h2>films</h2>
     <ul>
       <li v-for="film in store.films" :key="film.id">
         {{ film.title }} <br>
@@ -10,11 +11,18 @@
         {{ film.vote_average }}
       </li>
     </ul>
+    <h2>series</h2>
+    <ul>
+      <li v-for="serie in store.series">
+        {{ serie.name }}
+      </li>
+    </ul>
   </div>
 </template>
 
 <script>
 const endPointFilms = 'https://api.themoviedb.org/3/search/movie'
+const endPointSeries = 'https://api.themoviedb.org/3/search/tv'
 const api_key = 'f54a6d440796635ebf443de82f3b42de'
 import { store } from './components/data/store.js';
 import axios from 'axios';
@@ -50,11 +58,19 @@ export default {
         console.log(store.films)
       })
     },
+    fetchSeries(url) {
+      axios.get(url).then((res) => {
+        store.series = res.data.results
+        console.log(store.films)
+      })
+    },
 
     onSearchTerm(term) {
       this.filteredTerm = term;
-      const filterEndpoint = `${endPointFilms}?api_key=${api_key}&query=${this.filteredTerm}`
-      this.fetchFilms(filterEndpoint)
+      const filterEndpointFilms = `${endPointFilms}?api_key=${api_key}&query=${this.filteredTerm}`
+      const filterEndpointSeries = `${endPointSeries}?api_key=${api_key}&query=${this.filteredTerm}`
+      this.fetchFilms(filterEndpointFilms)
+      this.fetchSeries(filterEndpointSeries)
     },
   }
 }
--- src/components/data/store.js
+++ src/components/data/store.js
@@ -1,3 +1,6 @@
 import { reactive } from "vue";
 
-export const store = reactive({ films: [] });
+export const store = reactive({
+  films: [],
+  series: [],
+});
