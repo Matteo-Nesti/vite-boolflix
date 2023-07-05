e7ab748d6e5724b5bac478261cdd842fccaac8f7
Matteo-Nesti <matteo.nesti@hotmail.com>
2023-07-05 18:13:41 +0200

    add thumb image for films

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,24 @@
 <template>
   <div>
-    <input type="text" v-model="searchTerm">
+    <input type="text" v-model="searchTerm" @keyup.enter="onSearchTerm(searchTerm)">
     <button @click="onSearchTerm(searchTerm)">Cerca</button>
     <h2>films</h2>
     <ul>
       <li v-for="film in store.films" :key="film.id">
-        {{ film.title }} <br>
-        {{ film.original_title }} <br>
+        Titolo: {{ film.title }} <br>
+        Titolo originale: {{ film.original_title }} <br>
         <img :src="getImagePath(film.original_language)" :alt="film.original_language"><br>
-        {{ film.vote_average }}
+        Voto: {{ film.vote_average }}
+        <img :src="getThumbimage(film.poster_path)" :alt="film.title">
       </li>
     </ul>
     <h2>series</h2>
     <ul>
       <li v-for="serie in store.series">
-        {{ serie.name }} <br>
-        {{ serie.original_name }} <br>
+        Titolo: {{ serie.name }} <br>
+        Titolo originale: {{ serie.original_name }} <br>
         <img :src="getImagePath(serie.original_language)" :alt="serie.original_language"> <br>
-        {{ serie.vote_average }}
+        Voto: {{ serie.vote_average }}
       </li>
     </ul>
   </div>
@@ -26,7 +27,9 @@
 <script>
 const endPointFilms = 'https://api.themoviedb.org/3/search/movie'
 const endPointSeries = 'https://api.themoviedb.org/3/search/tv'
+const imageThumb = 'https://image.tmdb.org/t/p/'
 const api_key = 'f54a6d440796635ebf443de82f3b42de'
+
 import { store } from './components/data/store.js';
 import axios from 'axios';
 export default {
@@ -51,7 +54,9 @@ export default {
         return ''
       }
     },
-
+    getThumbimage(url) {
+      return imageThumb + 'w342' + url
+    },
 
     fetchFilms(url) {
       axios.get(url).then((res) => {
